eeb0363c5f88d37614359f384c2260e65860c746
Jatniel611 <52363239+Jatniel611@users.noreply.github.com>
2024-08-09 19:44:23 -0300

    config.py

--- config.py
+++ config.py
@@ -32,11 +32,11 @@ CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://xdmama:xv628MKL9nHdoNzW@avengers.ekg0n.mongodb.net/?retryWrites=true&w=majority&appName=avengers")
-CDB_NAME = environ.get("CDB_NAME", "xdmama")
+CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "mongodb+srv://xdmama:xv628MKL9nHdoNzW@avengers.ekg0n.mongodb.net/?retryWrites=true&w=majority&appName=avengers")
-DB_NAME = environ.get("DB_NAME", "xdmama")
+DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "1002173958953"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002173958953"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
